31bcab49677ff2bf3cd924a7eb35f67c8737701e
SBHARGAV2K <bhargav.sridhar@rwth-aachen.de>
2026-08-17 20:10:36 +0200

    Creating new module for visualizing, analyzing statistical concepts

---
+++ StatVizAnEval/binomial_distribution.py
@@ -0,0 +1,10 @@
+import scipy.stats as stats
+from statisics import statistics
+
+class BinomialDistribution:
+    def __init__(self, n, p):
+        self.n = n
+        self.p = p
+
+    def pmf(self, k):
+        return stats.binom.pmf(k, self.n, self.p)
